43a28451e976d8b5b3ddfdb93a667829865a422a
freegoleet <rorocoffeee@gmail.com>
2020-09-02 13:40:31 +0200

    SunMoon
    Updated sun moon stuff

--- Source/GP2/SunMoonComponent.cpp
+++ Source/GP2/SunMoonComponent.cpp
@@ -8,8 +8,7 @@ USunMoonComponent::USunMoonComponent() {
 
 void USunMoonComponent::BeginPlay() {
 	Super::BeginPlay();
-	sun->SetActorLocation(FVector(center->GetActorLocation().X + distanceFromCenter, center->GetActorLocation().Y, center->GetActorLocation().Z));
-	moon->SetActorLocation(FVector(center->GetActorLocation().X - distanceFromCenter, center->GetActorLocation().Y, center->GetActorLocation().Z));
+	
 
 	vActiveHeight.Z = activeHeight;
 	vInactiveHeight.Z = inactiveHeight;
@@ -17,16 +16,46 @@ void USunMoonComponent::BeginPlay() {
 	dayRot = FRotator(0, dayAngle, 0);
 	nightRot = FRotator(0, nightAngle, 0);
 
-	sunTargetLocation.Z = sun->GetActorLocation().Z + vActiveHeight.Z;
-	moonTargetLocation.Z = moon->GetActorLocation().Z + vInactiveHeight.Z;
 
 	dayNightTriggerComponent->onDay.AddDynamic(this, &USunMoonComponent::ToggleCelestials);
 	dayNightTriggerComponent->onNight.AddDynamic(this, &USunMoonComponent::ToggleCelestials);
 
-	UGameplayStatics::GetAllActorsWithTag(GetWorld(), FName(TEXT("CelestialCenter")), celestialsArray);
-	UGameplayStatics::GetAllActorsWithTag(GetWorld(), FName(TEXT("Sun")), celestialsArray);
-	UGameplayStatics::GetAllActorsWithTag(GetWorld(), FName(TEXT("Moon")), celestialsArray);
-		
+	GetOwner()->GetAttachedActors(celestialsArray);
+
+	for (AActor* celestial : celestialsArray) {
+		if(celestial->GetName() == "Sun")
+			sun = celestial;
+		if (celestial->GetName() == "Moon")
+			moon = celestial;
+	}
+
+	if (sun != nullptr && moon != nullptr) {
+		sun->GetAttachedActors(lightArray, false);
+		moon->GetAttachedActors(lightArray, false);
+	}
+
+	for (AActor* lightSource : lightArray) {
+		if (lightSource->GetName() == "SunLight") {
+			sunLightActor = lightSource;
+		}
+		if (lightSource->GetName() == "MoonLight") {
+			moonLightActor = lightSource;
+		}
+	}
+
+	
+
+	if (sunLightActor != nullptr && moonLightActor != nullptr) {
+		sunDirLight = sunLightActor->FindComponentByClass<UDirectionalLightComponent>();
+		moonDirLight = moonLightActor->FindComponentByClass<UDirectionalLightComponent>();
+	}
+
+	sun->SetActorLocation(FVector(center->GetActorLocation().X + distanceFromCenter, center->GetActorLocation().Y, center->GetActorLocation().Z));
+	moon->SetActorLocation(FVector(center->GetActorLocation().X - distanceFromCenter, center->GetActorLocation().Y, center->GetActorLocation().Z));
+
+	sunTargetLocation.Z = sun->GetActorLocation().Z + vActiveHeight.Z;
+	moonTargetLocation.Z = moon->GetActorLocation().Z + vInactiveHeight.Z;
+
 	sun->SetActorRelativeRotation(FRotator(0, 180, 0));
 
 }
@@ -45,14 +74,21 @@ void USunMoonComponent::ToggleCelestials(int dayNight) {
 		time = 0;
 		sunTargetLocation.Z = sun->GetActorLocation().Z + vActiveHeight.Z * 2;
 		moonTargetLocation.Z = moon->GetActorLocation().Z + vInactiveHeight.Z * 2;
+
+		sunTargetIntensity = sunActiveIntensity;
+		moonTargetIntensity = moonInactiveIntensity;
 	}
 	else if (dayNight == 1) {
 		targetRot.Yaw = nightRot.Yaw;
 		startRot = center->GetActorRotation();
 		dayOrNight = TestNight;
 		time = 0;
+
 		sunTargetLocation.Z = sun->GetActorLocation().Z + vInactiveHeight.Z * 2;
 		moonTargetLocation.Z = moon->GetActorLocation().Z + vActiveHeight.Z * 2;
+
+		sunTargetIntensity = sunInactiveIntensity;
+		moonTargetIntensity = moonActiveIntensity;
 	}
 
 }
@@ -64,5 +100,8 @@ void USunMoonComponent::MoveCelestials(float DeltaTime) {
 
 		sun->SetActorLocation(FMath::VInterpConstantTo(sun->GetActorLocation(), FVector(sun->GetActorLocation().X, sun->GetActorLocation().Y, sunTargetLocation.Z), DeltaTime, (activeHeight + -inactiveHeight) * 2 / duration));
 		moon->SetActorLocation(FMath::VInterpConstantTo(moon->GetActorLocation(), FVector(moon->GetActorLocation().X, moon->GetActorLocation().Y, moonTargetLocation.Z), DeltaTime, (activeHeight + -inactiveHeight) * 2 / duration));
+		
+		sunDirLight->SetIntensity(FMath::FInterpConstantTo(sunDirLight->Intensity, sunTargetIntensity, DeltaTime, (sunActiveIntensity + sunInactiveIntensity) * 2 / duration));
+		moonDirLight->SetIntensity(FMath::FInterpConstantTo(moonDirLight->Intensity, moonTargetIntensity, DeltaTime, (moonActiveIntensity + moonInactiveIntensity) * 2 / duration));
 	}
 }
